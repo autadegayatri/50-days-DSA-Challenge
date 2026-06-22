8fc7bfc3881c8d3402abf9c609356e0745abaa86
Gayatri Balasaheb Autade <autadegayatri56@gmail.com>
2026-06-22 18:17:40 +0530

    Day4

--- Day4/majorityElement.java
+++ Day4/majorityElement.java
@@ -2,36 +2,29 @@ import java.util.Arrays;
 class majorityElement {
     public static void main(String[] args) {
         majorityElement me = new majorityElement();
-        int[] nums = {3,2,3,4,4,4,0,0,1};
+        int[] nums = {3,4,4,4,4,4,0,0,1};
         System.out.println(me.majorityElement(nums));
     }
 
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        int maxf=1;
-        int ans=0;
-        if(nums.length==1){
-            return nums[0];
-        }
-        
-        for(int i=0;i<nums.length-1;i++){
-            int freq=1;
-            
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]==nums[j]){
-                    freq++;
-                }else{
-                    break;
+        return helper(nums,0,nums[0]);
+    }
 
-                }
+    private int helper(int[]nums, int s,int ref){
+        int count=1;
+        for(int i=s;i<nums.length;i++){
+            if(nums[i]==ref){
+                count++;
+            }else{
+                count--;
             }
-            if(freq>maxf){
-                ans=nums[i];
-                maxf=freq;
-
+            if(count==0){
+                return helper(nums,i,nums[i]);
             }
         }
-        return ans;
-        
+        return ref;
     }
 }
+
+    
+    
