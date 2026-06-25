7701b1dd161d6b992eac9b9ae7c6072651507da5
Gayatri Balasaheb Autade <autadegayatri56@gmail.com>
2026-06-25 22:43:26 +0530

    Day7

---
+++ Day7/permutation_in_string.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+class permutation_in_string {
+    public static void main(String[] args) {
+        permutation_in_string obj=new permutation_in_string();
+        String s1="ab";
+        String s2="eidbaooo";
+        System.out.println(obj.checkInclusion(s1,s2));
+    }
+    public boolean checkInclusion(String s1, String s2) {
+        int n=s1.length();
+        int m=s2.length();
+        if(n>m) return false;
+
+        int[]cnt1=new int[26];
+        int[]cnt2=new int[26];
+
+        for(int i=0;i<n;i++){
+            cnt1[s1.charAt(i)-'a']++;
+            cnt2[s2.charAt(i)-'a']++;
+        }
+        if(Arrays.equals(cnt1,cnt2)) return true;
+
+        int j=0;
+
+        for(int i=n;i<m;i++){
+            cnt2[s2.charAt(i)-'a']++;
+            cnt2[s2.charAt(j)-'a']--;
+
+            if(Arrays.equals(cnt1,cnt2)) return true;
+            j++;
+
+        }
+        return false;
+        
+    }
+}
